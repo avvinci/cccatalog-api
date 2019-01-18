9f75b044a7e23adda1abd12c630d762de0ade9c7
Alden Page <aldenstpage@gmail.com>
2019-01-18 15:14:52 -0500

    Delete Terraform infrastructure code (moved to ccsearch-infrastructure)

--- deployment/environments/dev/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cache-url" {
-  value = "${module.cacheserver.ec2-instance-url}"
-}
-
-/* This is defined in the branch elasticsearch_auto_reindex, which needs to be merged.
-output "ingestion-url" {
-  value = "${module.ingestion-server.ec2-instance-url}"
-}
-*/
